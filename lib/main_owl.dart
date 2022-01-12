f515ed677b25c7e2e6c09ca5a742fc36e811a853
Italo <62616261+italo532@users.noreply.github.com>
2022-01-12 20:03:11 -0300

    create build targets

---
+++ lib/main_owl.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_flavors/app_widget.dart';
+
+void main() => runApp(
+      const AppWidget(
+        flavor: 'owl',
+      ),
+    );
